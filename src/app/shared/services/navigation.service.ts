04e8bda8b55bba5a694077f55c3dbf702fd497bb
PRAJAMA <prabath.rajamani@mbusa.com>
2018-02-12 08:16:56 -0500

    menu items are updated.

--- src/app/shared/services/navigation.service.ts
+++ src/app/shared/services/navigation.service.ts
@@ -2,21 +2,21 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 interface IMenuItem {
-  type: string,       // Possible values: link/dropDown/icon/separator/extLink
-  name?: string,      // Used as display text for item and title for separator type
-  state?: string,     // Router state
-  icon?: string,      // Material icon name
-  tooltip?: string,   // Tooltip text
-  disabled?: boolean, // If true, item will not be appeared in sidenav.
-  sub?: IChildItem[], // Dropdown items
-  badges?: IBadge[]
+  type: string;       // Possible values: link/dropDown/icon/separator/extLink
+  name?: string;      // Used as display text for item and title for separator type
+  state?: string;     // Router state
+  icon?: string;      // Material icon name
+  tooltip?: string;   // Tooltip text
+  disabled?: boolean; // If true, item will not be appeared in sidenav.
+  sub?: IChildItem[]; // Dropdown items
+  badges?: IBadge[];
 }
 interface IChildItem {
-  type?: string,
-  name: string,       // Display text
-  state?: string,     // Router state
-  icon?: string,
-  sub?: IChildItem[]
+  type?: string;
+  name: string;       // Display text
+  state?: string;     // Router state
+  icon?: string;
+  sub?: IChildItem[];
 }
 
 interface IBadge {
@@ -26,9 +26,11 @@ interface IBadge {
 
 @Injectable()
 export class NavigationService {
-  constructor() { }
+  // Icon menu TITLE at the very top of navigation.
+  // This title will appear if any icon type item is present in menu.
+  iconTypeMenuTitle = 'Favorites';
 
-  defaultMenu: IMenuItem[] = [
+  iconMenu: IMenuItem[] = [
     {
       name: 'Favorite 1',
       type: 'icon',
@@ -171,11 +173,16 @@ export class NavigationService {
     }
   ];
 
-  // Icon menu TITLE at the very top of navigation.
-  // This title will appear if any icon type item is present in menu.
-  iconTypeMenuTitle: string = 'Frequently Accessed';
   // sets iconMenu as default;
-  menuItems = new BehaviorSubject<IMenuItem[]>(this.defaultMenu);
+  menuItems = new BehaviorSubject<IMenuItem[]>(this.iconMenu);
   // navigation component has subscribed to this Observable
   menuItems$ = this.menuItems.asObservable();
+
+  constructor() { }
+
+  // Customizer component uses this method to change menu.
+  // You can remove this method and customizer component.
+  // Or you can customize this method to supply different menu for
+  // different user type.
+  publishNavigationChange(menuType: string) {}
 }
