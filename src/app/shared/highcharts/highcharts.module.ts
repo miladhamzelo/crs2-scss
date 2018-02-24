a890aceb0ee61f9546e7efc586b40dcab7b2bd7e
dprabath <prabath.rajamani@mbusa.com>
2018-02-23 21:59:07 -0500

    Code formatted using prettier plugin.

--- src/app/shared/highcharts/highcharts.module.ts
+++ src/app/shared/highcharts/highcharts.module.ts
@@ -1,32 +1,27 @@
-import {NgModule} from '@angular/core';
-import {HighchartsStatic} from 'angular2-highcharts/dist/HighchartsService';
+import { NgModule } from "@angular/core";
+import { HighchartsStatic } from "angular2-highcharts/dist/HighchartsService";
 
 declare var require: any;
 export function highchartsFactory() {
-    const highcharts = require('highcharts');
-    const highchartsMore = require('highcharts/highcharts-more');
-    const highcharts3D = require('highcharts/highcharts-3d');
-    const highchartsExporting = require('highcharts/modules/exporting');
-    highchartsMore(highcharts);
-    highcharts3D(highcharts);
-    highchartsExporting(highcharts);
-    return highcharts;
+	const highcharts = require("highcharts");
+	const highchartsMore = require("highcharts/highcharts-more");
+	const highcharts3D = require("highcharts/highcharts-3d");
+	const highchartsExporting = require("highcharts/modules/exporting");
+	highchartsMore(highcharts);
+	highcharts3D(highcharts);
+	highchartsExporting(highcharts);
+	return highcharts;
 }
-import {ChartModule} from 'angular2-highcharts';
+import { ChartModule } from "angular2-highcharts";
 
 @NgModule({
-    imports: [
-        ChartModule
-    ],
-    providers: [
-        {
-            provide: HighchartsStatic,
-            useFactory: highchartsFactory
-        }
-    ],
-    exports: [
-        ChartModule
-    ]
+	imports: [ChartModule],
+	providers: [
+		{
+			provide: HighchartsStatic,
+			useFactory: highchartsFactory
+		}
+	],
+	exports: [ChartModule]
 })
-export class HighchartsModule { }
-
+export class HighchartsModule {}
--- src/app/shared/primeng/primeng.module.ts
+++ src/app/shared/primeng/primeng.module.ts
@@ -1,31 +1,34 @@
-import {NgModule} from '@angular/core';
+import { NgModule } from "@angular/core";
 import {
-    ButtonModule, ConfirmationService, ConfirmDialogModule, DataTableModule, FieldsetModule,
-    PanelModule, SharedModule
-} from 'primeng/primeng';
-import {SidebarModule} from 'primeng/components/sidebar/sidebar';
+	ButtonModule,
+	ConfirmationService,
+	ConfirmDialogModule,
+	DataTableModule,
+	FieldsetModule,
+	PanelModule,
+	SharedModule
+} from "primeng/primeng";
+import { SidebarModule } from "primeng/components/sidebar/sidebar";
 
 @NgModule({
-    imports: [
-        ButtonModule,
-        ConfirmDialogModule,
-        PanelModule,
-        FieldsetModule,
-        SidebarModule,
-        DataTableModule,
-        SharedModule
-    ],
-    exports: [
-        ButtonModule,
-        ConfirmDialogModule,
-        PanelModule,
-        FieldsetModule,
-        SidebarModule,
-        DataTableModule,
-        SharedModule
-    ],
-    providers: [
-        ConfirmationService
-    ]
+	imports: [
+		ButtonModule,
+		ConfirmDialogModule,
+		PanelModule,
+		FieldsetModule,
+		SidebarModule,
+		DataTableModule,
+		SharedModule
+	],
+	exports: [
+		ButtonModule,
+		ConfirmDialogModule,
+		PanelModule,
+		FieldsetModule,
+		SidebarModule,
+		DataTableModule,
+		SharedModule
+	],
+	providers: [ConfirmationService]
 })
-export class PrimengModule { }
+export class PrimengModule {}
